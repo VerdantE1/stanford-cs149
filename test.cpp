eb93a91d607746ced513442e3981123fbd9437be
Frank <2523877046@qq.com>
2025-03-02 10:19:00 +0800

    init

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Buzzlight on 2025/3/2.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by Buzzlight on 2025/3/2.
+//
+
+#ifndef STANFORD_CS149_TEST_H
+#define STANFORD_CS149_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //STANFORD_CS149_TEST_H
